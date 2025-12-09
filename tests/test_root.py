2844beef4b15d7dfed06b1842ce9f88ca8d3718f
Jay <jay.rathod@techabbot.com>
2025-12-09 15:56:26 +0530

    `Removed test_root_endpoint_server test and corresponding comment from .gitignore`

--- tests/test_root.py
+++ tests/test_root.py
@@ -17,18 +17,18 @@ def test_root():
     assert re.search(r"running|ready|healthy", msg)
 
 
-def test_root_endpoint_server():
-    try:
-        response = httpx.get("http://localhost:8000/")
-    except Exception:
-        pytest.fail("❌ Server is not running or refused the connection")
+# def test_root_endpoint_server():
+#     try:
+#         response = httpx.get("http://localhost:8000/")
+#     except Exception:
+#         pytest.fail("❌ Server is not running or refused the connection")
 
-    assert response.status_code == 200
-    data = response.json()
+#     assert response.status_code == 200
+#     data = response.json()
 
-    assert "status" in data
-    assert isinstance(data["status"], str)
-    assert len(data["status"]) > 0
+#     assert "status" in data
+#     assert isinstance(data["status"], str)
+#     assert len(data["status"]) > 0
 
 def test_prediction():
     response = client.post("/predict", params={"text": "This is good"})
